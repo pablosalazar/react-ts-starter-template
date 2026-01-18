6330f9b7d58f56e44405809a42c1c145604813f6
Juan Pablo Salazar <pablo.salazar.restrepo@gmail.com>
2026-01-18 10:07:06 -0500

    chore: setup husky and commitlint for git hooks

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation changes
+        "style", // Code style changes (formatting)
+        "refactor", // Code refactoring
+        "perf", // Performance improvements
+        "test", // Adding tests
+        "chore", // Maintenance tasks
+        "ci", // CI/CD changes
+        "build", // Build system changes
+        "revert", // Revert a commit
+      ],
+    ],
+  },
+};
